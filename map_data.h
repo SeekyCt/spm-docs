5099d71ac81f8a077d95c54e7821ebfd506ac956
Seeky <58006653+SeekyCt@users.noreply.github.com>
2020-06-02 14:53:06 +0100

    A small bit of map stuff

---
+++ map_data.h
@@ -0,0 +1,12 @@
+#DEFINE MAP_ID_MAX 0x1d4
+
+typedef struct {
+  char * name;
+  char * nameAgain; // initialised as the exact same pointer, maybe changed later on?
+  // unknown 0x8-17
+  void * somethingInRel; // Set by rel prolog function
+} MapData;
+
+static MapData ** MapDataList; // 804031b8
+
+MapData * getMapData(char * mapName); // 800294e0
