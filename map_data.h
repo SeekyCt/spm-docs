4ffff578c721e6a585a95eebd5000924fbf642df
Seeky <58006653+SeekyCt@users.noreply.github.com>
2020-08-26 19:10:12 +0100

    Update map_data.h

--- map_data.h
+++ map_data.h
@@ -6,8 +6,17 @@ typedef struct {
   // unknown 0x8-17
   void * evtScript; // In rel, linked by prolog function. See https://github.com/PistonMiner/ttyd-tools
 } MapData;
+MapData * mapDataList[MAP_ID_MAX]; // 804031b8
 
-static MapData ** MapDataList; // 804031b8
+typedef struct {
+  char areaName[3]; // always(?) first 3 characters of map name
+  // unknown 0x3-1f
+  char mapName[32];
+  char doorName[32]; // door the map was entered with
+  EvtEntry * evtEntry;
+  // unknown 0x64-73
+} MapHandler;
+MapHandler * mapHandler; // 805ae0a8
 
-MapData * getMapData(char * mapName); // 800294e0
-void * getMapEvtScript(char * mapName); // 800295a8
+MapData * mapDataPtr(char * mapName); // 800294e0
+void * mapDataEvtPtr(char * mapName); // 800295a8
