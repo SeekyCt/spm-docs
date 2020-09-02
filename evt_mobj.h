cd358a5a488f686916e5fb74ab4c978ac271497c
Seeky <58006653+SeekyCt@users.noreply.github.com>
2020-09-02 19:03:20 +0100

    Create evt_mobj.h

---
+++ evt_mobj.h
@@ -0,0 +1,51 @@
+/*
+  Functions used to spawn map objects with the user_func instruction in evt scripts, and the functions the objects use internally
+*/
+
+/*
+  evt_mobj_blk - spawns various blocks (question, brick & variants)
+
+  1st param: int type
+    0 = brick block
+    1 = question block
+    the rest unknown
+  2nd param: char * instanceName
+  3rd param: float x
+  4th param: float y
+  5th param: float z
+  6th param: ChanceEntry * chanceTable
+    Format is an array of the following struct, terminated by one with an id of 0
+      struct ChanceEntry {
+        int itemId;
+        int chance;
+      }
+  7th param: unknown
+  8th param: unknown
+*/
+int evt_mobj_blk(EvtEntry * entry); // 800f8be8
+int chanceTableNext(MOBJEntry * entry); // 800f6754 - returns a random id from the table
+
+/*
+  evt_mobj_kan - spawns a sign
+  1st param: int rotation
+    0 for facing 2d camera
+    1 for perpendicular to 2d camera
+  2nd param: char * instanceName
+  3rd param: float x
+  4th param: float y
+  5th param: float z
+  6th param: EvtScript * interactScript
+  7th param: unknown
+*/
+int evt_mobj_kan(EvtEntry * entry); // 800f6440
+
+/*
+  evt_mobj_save_blk - spawns a save block
+  1st param: char * instanceName
+  2nd param: float x
+  3rd param: float y
+  4th param: float z
+  5th param: EvtScript * interactScript
+*/
+
+int evt_mobj_save_blk(EvtEntry * entry); // 800fb118
