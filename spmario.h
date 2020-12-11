e2ecf30450367a0485c24f640a1decf7cf0a6de8
Seeky <seeky.mkw@gmail.com>
2020-12-11 18:15:31 +0000

    Create spmario.h

---
+++ spmario.h
@@ -0,0 +1,14 @@
+typedef struct {
+    // unknown 0x0-f7
+    int64_t time;
+    // unknown 0x100-13f
+    int gsw0; // the first gsw is larger
+    uint8_t gswf[1024]; // evt global saved work flags
+    uint8_t gsw[2048]; // evt global saved work, index 0 is unused in favour of gsw0
+    uint8_t lswf[64]; // evt local saved work flags
+    uint8_t lsw[1024]; // evt local saved work
+    // unknown 0x1184-1ab0
+} MarioStGlobals;
+
+MarioStGlobals * gp; // 805ae178
+MarioStGlobals marioSt; // 80525550
