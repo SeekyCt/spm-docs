a8bef672eab4be851eb90e65f224cdac966d59b5
Seeky <seeky.mkw@gmail.com>
2021-03-26 23:17:41 +0000

    Gravity rotation

--- setup_data.h
+++ setup_data.h
@@ -10,7 +10,8 @@
 
 typedef struct {
 /* 0x00 */ int instanceId; // ignored if 0
-/* 0x04 */ // 0x4-5f
+/* 0x04 */ // 0x4-5b
+/* 0x5C */ float gravityRotation; // degrees anti-clockwise about the z-axis
 } MiscSetupDataV6; // total size 0x60
 
 typedef struct {
