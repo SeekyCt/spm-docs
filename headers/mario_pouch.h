f8ab8f63206e5fa7256c57a3908deaede13166ba
Seeky <seeky.mkw@gmail.com>
2021-06-10 14:22:07 +0100

    Added more MarioPouchWork fields

--- headers/mario_pouch.h
+++ headers/mario_pouch.h
@@ -19,16 +19,22 @@ typedef struct {
 /* 0x2 */ s16 itemType; // see items.txt
 } PouchCharOrPixlInfo; // total size 0x4
 
+typedef struct {
+/* 0x00 */ s32 characterId;
+/* 0x04 */ s32 score;
+/* 0x08 */ // unknown 0x8-17
+} PouchMinigameScore; // total size 0x18
+
 /*
     This struct is copied in full to & from the save file
 */
 typedef struct {
-/* 0x000 */ // unknown 0x0-3
+/* 0x000 */ // unknown 0x0-3, unused?
 /* 0x004 */ s32 level;
 /* 0x008 */ s32 attack;
 /* 0x00C */ s32 hp;
 /* 0x010 */ s32 maxHp;
-/* 0x014 */ // unknown 0x14-17
+/* 0x014 */ s32 flipTimer;
 /* 0x018 */ s32 xp;
 /* 0x01C */ s32 coins;
 /* 0x020 */ s16 keyItem[32];
@@ -36,12 +42,28 @@ typedef struct {
 /* 0x074 */ s16 shopItem[32];
 /* 0x0B4 */ PouchCharOrPixlInfo characters[4];
 /* 0x0C4 */ PouchCharOrPixlInfo pixls[16];
-/* 0x104 */ // unknown 0x104-68b
+/* 0x104 */ s32 shopPoints;
+/* 0x108 */ u32 shopPointRewardsCollected; // bit mask, index 1 << i
+/* 0x10C */ s8 catchCards[256]; // value is the amount currently owned, index is item id minus the first card item id (282)
+/* 0x20C */ // unknown 0x20c-30b
+/* 0x30C */ u32 ownedMaps[2]; // maps currently in inventory, index [id / 32] & 1 << (id % 32)
+/* 0x314 */ u32 foundMaps[2]; // maps with rewards claimed, index [id / 32] & 1 << (id % 32)
+/* 0x31C */ u32 knownRecipes[3]; // index [id / 32] & 1 << (id % 32)
+/* 0x328 */ u32 knownCards[8]; // flags indicating if a card was ever collected, index [id / 32] & 1 << (id % 32)
+/* 0x348 */ s32 charmsRemaining;
+/* 0x34C */ s32 killsBeforeNextCharm;
+/* 0x350 */ // unknown 0x350-367
+/* 0x368 */ PouchMinigameScore minigameScores[4][5];
+/* 0x548 */ // unknown 0x548-687
+/* 0x688 */ s32 arcadeTokens;
 /* 0x68C */ s32 totalCoinsCollected;
-/* 0x690 */ // unknown 0x690-697
+/* 0x690 */ s32 maxJumpCombo;
+/* 0x694 */ s32 maxStylishCombo;
 /* 0x698 */ s32 enemiesDefeated;
 /* 0x69C */ // unknown 0x69c-69f
 } MarioPouchWork; // total size 0x6a0
+
+MarioPouchWork marioPouchWork; // 80511a28
 
 /*
     Returns a pointer to the MarioPouchWork instance
