7549e23b57eb2a51d5379b38c5e09b81b7b17b25
Seeky <seeky.mkw@gmail.com>
2021-06-10 18:15:58 +0100

    Pouch function documentation

--- headers/mario_pouch.h
+++ headers/mario_pouch.h
@@ -37,29 +37,36 @@ typedef struct {
 /* 0x014 */ s32 flipTimer;
 /* 0x018 */ s32 xp;
 /* 0x01C */ s32 coins;
-/* 0x020 */ s16 keyItem[32];
-/* 0x060 */ s16 useItem[10];
-/* 0x074 */ s16 shopItem[32];
+/* 0x020 */ s16 keyItem[32]; // key items
+/* 0x060 */ s16 useItem[10]; // usable items
+/* 0x074 */ s16 shopItem[32]; // items stored by the player at a shop
 /* 0x0B4 */ PouchCharOrPixlInfo characters[4];
 /* 0x0C4 */ PouchCharOrPixlInfo pixls[16];
 /* 0x104 */ s32 shopPoints;
 /* 0x108 */ u32 shopPointRewardsCollected; // bit mask, index 1 << i
 /* 0x10C */ s8 catchCards[256]; // value is the amount currently owned, index is item id minus the first card item id (282)
 /* 0x20C */ // unknown 0x20c-30b
-/* 0x30C */ u32 ownedMaps[2]; // maps currently in inventory, index [id / 32] & 1 << (id % 32)
-/* 0x314 */ u32 foundMaps[2]; // maps with rewards claimed, index [id / 32] & 1 << (id % 32)
-/* 0x31C */ u32 knownRecipes[3]; // index [id / 32] & 1 << (id % 32)
-/* 0x328 */ u32 knownCards[8]; // flags indicating if a card was ever collected, index [id / 32] & 1 << (id % 32)
-/* 0x348 */ s32 charmsRemaining;
-/* 0x34C */ s32 killsBeforeNextCharm;
+
+            // where x is the item id minus the first treasure map item id (234):
+/* 0x30C */ u32 ownedMaps[2]; // maps currently in inventory, index [x / 32] & 1 << (x % 32)
+/* 0x314 */ u32 foundMaps[2]; // maps with rewards claimed, index [x / 32] & 1 << (x % 32)
+
+            // where x is the item id minus the first cooked item id (120):
+/* 0x31C */ u32 knownRecipes[3]; // flags indicating if a recipe was ever cooked/collected, index [x / 32] & 1 << (x % 32)
+
+            // where x is the item id minus the first card item id (282):
+/* 0x328 */ u32 knownCards[8]; // flags indicating if a card was ever collected, index [x / 32] & 1 << (x % 32)
+
+/* 0x348 */ s32 charmsRemaining; // Merlee charms remaining from current purchase
+/* 0x34C */ s32 killsBeforeNextCharm; // kills before the next Merlee charm triggers
 /* 0x350 */ // unknown 0x350-367
 /* 0x368 */ PouchMinigameScore minigameScores[4][5];
 /* 0x548 */ // unknown 0x548-687
 /* 0x688 */ s32 arcadeTokens;
-/* 0x68C */ s32 totalCoinsCollected;
+/* 0x68C */ s32 totalCoinsCollected; // all-time coins collected
 /* 0x690 */ s32 maxJumpCombo;
 /* 0x694 */ s32 maxStylishCombo;
-/* 0x698 */ s32 enemiesDefeated;
+/* 0x698 */ s32 enemiesDefeated; // all-time enemies defeated
 /* 0x69C */ // unknown 0x69c-69f
 } MarioPouchWork; // total size 0x6a0
 
@@ -77,7 +84,12 @@ MarioPouchWork * pouchGetPtr(); // 8014c088
 void pouchInit(); // 8014c094
 void pouchReInit(); // 8014c728
 
+/*
+    Updates 3d flip timer
+*/
 void pouchMain(); // 8014cd90
+
+// 8014d184 unknown function
 
 /*
     Set/get the player's level
@@ -119,11 +131,49 @@ void pouchAddXp(s32 increase); // 8014d508
 /*
     Set/get/add to the player's coin count
 */
-void pouchSetCoins(s32 coins); // 8014d548
-s32 pouchGetCoins(); // 8014d57c
-void pouchAddCoins(s32 increase); // 8014d58c, increases totalCoinsCollected
+void pouchSetCoin(s32 coins); // 8014d548
+s32 pouchGetCoin(); // 8014d57c
+void pouchAddCoin(s32 increase); // 8014d58c, increases totalCoinsCollected
 
+/*
+    Adds the specified item to its corresponding inventory (can't be used for shop items)
+    Returns whether it was successfully added
+*/
+bool pouchAddItem(s32 itemId); // 8014d5f8
+
+/*
+    Checks whether the specified item id is in its corresponding inventory (can't be used for shop items)
+*/
+bool pouchCheckHaveItem(s32 itemId); // 8014dbf4
+
+/*
+    Removes the first item with the specified id in its corresponding inventory (can't be used for shop items)
+*/
+void pouchRemoveItem(s32 itemId); // 8014d5f8
+
+/*
+    Removes the with the specified id at the specified index in its corresponding inventory
+    Only supports keyItem and useItem
+    Asserts that the id of the item at the index is what was passed in
+*/
 void pouchRemoveItemIdx(s32 itemId, s32 idx); // 8014e6d0
+
+/*
+    Adds the specified item id to the player's shop stored inventory
+    Returns whether it was successfully added
+*/
+bool pouchAddShopItem(s32 id); // 8014ea38
+
+/*
+    Removes the first item with the specified id in the player's shop stored inventory
+*/
+void pouchRemoveShopItem(); // 8014eb68
+
+/*
+    Removes the with the specified id at the specified index in the player's shop stored inventory
+    Asserts that the id of the item at the index is what was passed in
+*/
+void pouchRemoveShopItemIdx(); // 
 
 /*
     Get the information about a character slot
@@ -131,9 +181,10 @@ void pouchRemoveItemIdx(s32 itemId, s32 idx); // 8014e6d0
 PouchCharOrPixlInfo * pouchGetCharInfo(s32 slot); // 8014ef98
 
 /*
-    Make a character selectable by id, if it exists in a slot
+    Make a character selectable/not by id, if it exists in a slot
 */
 void pouchMakeCharSelectable(s32 itemId); // 8014efb0
+void pouchMakeCharNotSelectable(s32 itemId); // 8014f018
 
 /*
     Get the information about a pixl slot
@@ -149,7 +200,7 @@ void pouchMakePixlNotSelectable(s32 itemId); // 8014f154
 /*
     Check if a pixl is selected by id
 */
-void pouchIsPixlSelected(s32 itemId); // 8014f210
+void pouchCheckPixlSelected(s32 itemId); // 8014f210
 
 /*
     Returns the id of the currently selected pixl
@@ -163,15 +214,58 @@ s32 pouchGetCurPixl(); // 8014f31c
 void pouchSetPixlSelected(s32 itemId); // 8014f408
 
 /*
+    Turns on the foundMaps/knownRecipes bitflag for the specified item id
+*/
+void pouchRegisterMapFound(s32 itemId); // 8014f594
+void pouchRegisterRecipeKnown(s32 itemId); // 8014f64c
+
+/*
+    Returns whether the specified recipe item id has ever been cooked/collected
+*/
+bool pouchCheckRecipeKnown(s32 itemId); // 8014f704
+
+/*
+    Returns the amount of the specified card in the inventory
+*/
+s32 pouchGetCardCount(s32 itemId); // 8014f7c8
+
+/*
+    Returns whether the specified card item id has ever been collected
+*/
+bool pouchCheckCardKnown(s32 itemId); // 8014f84c
+
+/*
+    Takes a jump/stylish combo length and updates maxJumpCombo/maxStylishCombo if it's higher
+*/
+void pouchRegisterJumpCombo(s32 length); // 8014f910
+void pouchRegisterStylishCombo(s32 length); // 8014f944
+
+/*
+    Updates all-time enemies defeated count
+*/
+void pouchSetEnemiesDefeated(s32 count); // 8014f978
+
+/*
     Returns how many coins have ever been collected on this save
 */
 s32 pouchGetTotalCoinsCollected(); // 8014f99c
 
 /*
-    Returns/sets how many enemies have ever been defeated on this save
+    Returns the longest jump/stylish combo length recorded
+*/
+s32 pouchGetMaxJumpCombo(); // 8014f9ac
+s32 pouchGetMaxStylishCombo(); // 8014f9bc
+
+/*
+    Returns all-time enemies defeated count
 */
 s32 pouchGetEnemiesDefeated(); // 8014f9cc
-void pouchSetEnemiesDefeated(s32 totalDefeated); // 8014f978
+
+/*
+    Returns/sets the number of arcade tokens in the inventory
+*/
+s32 pouchGetArcadeTokens(); // 8014f9dc
+void pouchSetArcadeTokens(int count); // 8014f9ec
 
 /*
     Returns the number of useItem/keyItem/shopItem/char/pixl slots that aren't empty [and aren't selected]
@@ -188,4 +282,13 @@ s32 pouchCountPixlsNotSelected(); // 8014fd18
 */
 bool pouchCheckFreeUseItem(); // 8014fdb0
 
-// a lot of unknown functions
+/*
+    Returns the number of treasure maps currently in the inventory
+*/
+s32 pouchCountMapsOwned(); // 8014fe88
+
+/*
+    Returns the number of recipes / cards ever collected
+*/
+s32 pouchCountRecipesKnown(); // 8014ff9c
+s32 pouchCountCardsKnown(); // 801500b0
