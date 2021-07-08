903d1070015357507453b92ae51b1e90a969c23c
Seeky <seeky.mkw@gmail.com>
2021-07-08 21:07:10 +0100

    Update item_data.h

--- headers/item_data.h
+++ headers/item_data.h
@@ -5,13 +5,15 @@
 
 typedef struct {
 /* 0x00 */ const char * itemName;
-/* 0x04 */ s16 itemId;
+/* 0x04 */ s16 iconId;
 /* 0x06 */ // unknown 0x6-7
 /* 0x08 */ const char * animPoseName;
 /* 0x0C */ // unknown 0xc-f
 /* 0x10 */ const char * nameMsg;
 /* 0x14 */ const char * descMsg;
-/* 0x18 */ // unknown 0x18-2c
+/* 0x18 */ // unknown 0x18-1d
+/* 0x1E */ s16 xpGain;
+/* 0x20 */ // unknown 0x20-2b
 } ItemData; // total size 0x2c
 
 ItemData itemDataTable[538]; // 803f5f98
